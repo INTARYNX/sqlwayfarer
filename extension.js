45f91f9e0c5558adcd234c437f561d2fa7c0eea4
INTARYNX <nifournier@outlook.com>
2025-07-19 17:03:35 +0200

    Added Icon on the VSCode left bar

--- extension.js
+++ extension.js
@@ -4,28 +4,67 @@ const vscode = require('vscode');
 const SqlWayfarerPanel = require('./panels/SqlWayfarerPanel');
 
 /**
+ * Provider minimal pour que l'icône apparaisse
+ */
+class SqlWayfarerViewProvider {
+    getTreeItem(element) {
+        return element;
+    }
+
+    getChildren(element) {
+        if (!element) {
+            // Retourne un élément simple avec une action
+            const item = new vscode.TreeItem('SQL Wayfarer', vscode.TreeItemCollapsibleState.None);
+            item.iconPath = new vscode.ThemeIcon('compass');
+            item.description = 'Click to open';
+            item.command = {
+                command: 'sqlwayfarer.sqlwayfarer',
+                title: 'Open SQL Wayfarer'
+            };
+            return [item];
+        }
+        return [];
+    }
+}
+
+/**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-    console.log('Congratulations, your extension "sqlwayfarer" is now active!');
+    console.log('SQL Wayfarer extension is now active!');
 
-    // Original Hello World command
-    const helloWorldDisposable = vscode.commands.registerCommand('sqlwayfarer.helloWorld', function () {
-        vscode.window.showInformationMessage('Hello World from sqlwayfarer!');
-    });
+    // Enregistrer le provider pour que l'icône apparaisse
+    const viewProvider = new SqlWayfarerViewProvider();
+    vscode.window.registerTreeDataProvider('sqlWayfarerView', viewProvider);
 
-    // SQL Wayfarer command
+    // Commande principale SQL Wayfarer avec fermeture automatique
     const sqlWayfarerDisposable = vscode.commands.registerCommand('sqlwayfarer.sqlwayfarer', function () {
-        console.log('sqlwayfarer.sqlwayfarer command executed');
+        console.log('Opening SQL Wayfarer...');
+        
+        // Ouvrir SQL Wayfarer
         SqlWayfarerPanel.createOrShow(context.extensionUri, context);
+        
+        // Fermer automatiquement la sidebar après un court délai
+        setTimeout(() => {
+            vscode.commands.executeCommand('workbench.action.closeSidebar');
+        }, 200);
     });
 
-    context.subscriptions.push(helloWorldDisposable);
+    // Original Hello World command (garder pour compatibilité si nécessaire)
+    const helloWorldDisposable = vscode.commands.registerCommand('sqlwayfarer.helloWorld', function () {
+        vscode.window.showInformationMessage('Hello World from SQL Wayfarer!');
+    });
+
+    // Enregistrer les commandes
     context.subscriptions.push(sqlWayfarerDisposable);
-    console.log('sqlwayfarer extension with SQL Wayfarer activated successfully');
+    context.subscriptions.push(helloWorldDisposable);
+
+    console.log('SQL Wayfarer extension activated successfully');
 }
 
-function deactivate() {}
+function deactivate() {
+    console.log('SQL Wayfarer extension deactivated');
+}
 
 module.exports = {
     activate,
